2f2f0609f3a60b26207b745cd9d93ac8407882f5
stepan881 <stepanstepanov180@gmail.com>
2020-03-26 11:04:14 +0300

    Создание проекта. Вывод в консоль. Урок №1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var money = 10;
+var income = 5;
+var addExpenses = 8;
+var deposit = 100;
+var mission = 'complete';
+var period = '30 day';
+
+alert(period + ' ' + mission);
+
+console.log(deposit - money + '$ осталось');
