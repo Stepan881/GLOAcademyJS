17c1e260d180e35ef0df104f35fb593ccf31a9e4
stepan881 <stepanstepanov180@gmail.com>
2020-03-30 14:24:33 +0300

    Динамическая типизация урок 3

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-const money = 100;
+let money = 100;
 const income = 'фриланс';
-const addExpenses = 'Интернет, Такси, Коммуналка';
-const deposit = true;
-const mission = 1000;
+let addExpenses = 'Интернет, Такси, Коммуналка';
+let deposit = true;
+const mission = 100000;
 const period = 6;
 
 console.log('money: ', typeof money);
@@ -20,3 +20,33 @@ let budgetDay;
 
 budgetDay = money / 30;
 console.log('budgetDay: ', budgetDay);
+
+// lesson3
+
+money = prompt('Ваш месячный доход', '10000');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Интернет, Такси, Коммуналка');
+deposit = confirm('Есть ли у вас депозит в банке?');
+
+let expenses1 = prompt('Введите обязательную статью расходов?', 'Интернет');
+let amount1 =  prompt('Во сколько это обойдется?', '1000');
+let expenses2 = prompt('Введите обязательную статью расходов?', 'Такси');
+let amount2 = prompt('Во сколько это обойдется?', '2000');
+
+let budgetMonth = parseInt(money - (parseInt(amount1) + parseInt(amount2)));
+console.log('Бюджет на месяц: ', budgetMonth);
+
+console.log('Цель будет достигната за: ' + Math.ceil(mission / budgetMonth) + ' месяца');
+
+console.log('Бюджет на месяц: ' + budgetMonth);
+budgetDay = Math.floor(budgetMonth / 30)
+console.log('Бюджет на день: ' + budgetDay);
+
+if (budgetDay >= 1200) {
+  console.log('У вас высокий уровень дохода');
+} else if (budgetDay >= 600 && budgetDay < 1200) {
+  console.log('У вас средний уровень дохода');
+} else if (budgetDay < 600) {
+  console.log('К сожалению у вас уровень дохода ниже среднего');
+} else if (budgetDay <= 0) {
+  console.log('Что то пошло не так');
+}
