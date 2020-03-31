1f481eab8a082b60c629ad69efc73369f6ddb714
stepan881 <stepanstepanov180@gmail.com>
2020-03-31 10:40:12 +0300

    УРОК №4 Все о функциях

--- js/main.js
+++ js/main.js
@@ -1,27 +1,11 @@
+"use strict";
+
 let money = 100;
 const income = 'фриланс';
 let addExpenses = 'Интернет, Такси, Коммуналка';
 let deposit = true;
 const mission = 100000;
 const period = 6;
-
-console.log('money: ', typeof money);
-console.log('income: ',typeof income);
-console.log('deposit: ',typeof deposit);
-
-console.log('addExpenses: ', addExpenses.length);
-
-console.log(`Период равен ${period} месяцев`);
-console.log(`Цель заработать ${mission} рублей`);
-
-console.log(addExpenses.toLowerCase().split(', '));
-
-let budgetDay;
-
-budgetDay = money / 30;
-console.log('budgetDay: ', budgetDay);
-
-// lesson3
 
 money = prompt('Ваш месячный доход', '10000');
 addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Интернет, Такси, Коммуналка');
@@ -32,14 +16,22 @@ let amount1 =  prompt('Во сколько это обойдется?', '1000');
 let expenses2 = prompt('Введите обязательную статью расходов?', 'Такси');
 let amount2 = prompt('Во сколько это обойдется?', '2000');
 
-let budgetMonth = parseInt(money - (parseInt(amount1) + parseInt(amount2)));
-console.log('Бюджет на месяц: ', budgetMonth);
 
-console.log('Цель будет достигната за: ' + Math.ceil(mission / budgetMonth) + ' месяца');
+function getExpensesMonth(sum1, sum2) {
+  return parseInt(sum1) + parseInt(sum2);
+}
 
-console.log('Бюджет на месяц: ' + budgetMonth);
-budgetDay = Math.floor(budgetMonth / 30)
-console.log('Бюджет на день: ' + budgetDay);
+function getAccumulatedMonth() {
+  return money - getExpensesMonth(amount1, amount2);
+}
+
+let accumulatedMonth = getAccumulatedMonth();
+
+function getTargetMonth(mission, accumulatedMonth) {
+  return Math.floor(mission / accumulatedMonth);
+}
+
+let budgetDay = Math.floor(accumulatedMonth / 30);
 
 if (budgetDay >= 1200) {
   console.log('У вас высокий уровень дохода');
