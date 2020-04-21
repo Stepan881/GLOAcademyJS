f226962a8f2a3235e93a890ba0adc880f86332e6
stepan881 <stepanstepanov180@gmail.com>
2020-04-21 12:57:05 +0300

    lesson19, paramsDocument Scroll

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 'use strict';
 
 window.addEventListener("DOMContentLoaded", () => {
+    // Плавная прокрутка
+    const scrolling = (el) => {
+        if (el.href === undefined) return;
+        let link = el.href.split('#')[1];
+        document.querySelector('#'+link).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    };
+
   // таймер
   function countTimer() {
     let timerHours = document.querySelector('#timer-hours');
@@ -36,4 +46,76 @@ window.addEventListener("DOMContentLoaded", () => {
   }
   countTimer();
 
+
+  // меню
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector('.menu');
+    const menu = document.querySelector('menu');
+    const closeBtn = document.querySelector('.close-btn');
+    const menuItem = menu.querySelectorAll('ul>li');
+    
+    let count = -100;
+    const animate = () => {
+        if (document.documentElement.clientWidth < 768) {
+            menu.style.transform = `translate(0)`;
+            return;
+        }
+        let requestId = requestAnimationFrame(animate);
+        count += 2;
+        menu.style.transform = `translate(${count}%)`;
+        if (count === 0) {
+            cancelAnimationFrame(requestId);
+        }
+    };
+
+    const handlerMenu = (evt) => {
+        evt.preventDefault();
+        scrolling(evt.target);
+        if (!menu.style.transform || menu.style.transform === `translate(-100%)`) {
+            // menu.style.transform = `translate(0)`;
+            count = -100;
+            animate();
+        } else {
+            menu.style.transform = `translate(-100%)`;
+        }  
+    };
+
+    btnMenu.addEventListener('click', handlerMenu);
+    closeBtn.addEventListener('click', handlerMenu);
+
+    menuItem.forEach(el => {
+        el.addEventListener('click', handlerMenu);
+    });
+
+  };
+  toggleMenu();
+ 
+
+ // popup
+  const togglePopup = () => {
+    const popup = document.querySelector('.popup');
+    const popupBtn = document.querySelectorAll('.popup-btn');
+    const popupClose = document.querySelector('.popup-close');
+
+    popupBtn.forEach(el => {
+        el.addEventListener('click', () => {
+            popup.style.display = 'block';
+        });
+    });
+
+    popupClose.addEventListener('click', () => {
+        popup.style.display = 'none';
+    });
+  };
+  togglePopup();
+
+  // Плавная прокрутка
+  const scrolHead = () => {
+    const btnScrolling = document.querySelector('a[href="#service-block"]');
+    btnScrolling.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        scrolling(btnScrolling);
+    });
+  };
+  scrolHead();
 });
