0cb05e589b675372f40b04cb0dd9ef0e59f00f9f
Stepan881 <stepanstepanov180@gmail.com>
2020-04-30 01:21:25 +0300

    loader

--- js/main.js
+++ js/main.js
@@ -370,9 +370,226 @@ window.addEventListener("DOMContentLoaded", () => {
     const loadMessage = 'Загрузка...';
     const successMessage = 'Спасибо! Мы с вами свяжемся!';
 
+    const bodyHtml = document.querySelector('body');
     const forms = document.querySelectorAll('form');
     const statusMessage = document.createElement('div');
-    statusMessage.style.cssText = `font-size: 2rem;`;
+          statusMessage.classList.add('status-message');
+          statusMessage.style.cssText = `font-size: 2rem;`;
+
+    const loader = () => {
+      return (`<style>
+                .preloader__container {
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                  height: 100vh;
+                  width: 100%;
+                  overflow: hidden;
+                  animation-delay: 1s;
+                  background-color: rgba(0,0,0,0.33);
+                  
+                  position: absolute;
+                  left: 0;
+                  top: 0;
+
+                }
+
+                .item-1 {
+                  width: 20px;
+                  height: 20px;
+                  background: #f583a1;
+                  border-radius: 50%;
+
+                  margin: 7px;
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                }
+                @keyframes scale {
+                  0% {
+                    transform: scale(1);
+                  }
+                  50%,
+                    75% {
+                    transform: scale(2.5);
+                  }
+                  78%, 100% {
+                    opacity: 0;
+                  }
+                }
+                .item-1:before {
+                  content: '';
+                  width: 20px;
+                  height: 20px;
+                  border-radius: 50%;
+                  background-color: #eed968;
+                  opacity: 0.7;
+                  animation: scale 2s infinite cubic-bezier(0, 0, 0.49, 1.02);
+                  animation-delay: 200ms;
+                  transition: 0.5s all ease;
+                  transform: scale(1);
+                }
+
+                .item-2 {
+                  width: 20px;
+                  height: 20px;
+                  border-radius: 50%;
+                  background-color: #eece68;
+                  margin: 7px;
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                }
+                @keyframes scale {
+                  0% {
+                    transform: scale(1);
+                  }
+                  50%,
+                    75% {
+                    transform: scale(2.5);
+                  }
+                  78%, 100% {
+                    opacity: 0;
+                  }
+                }
+                .item-2:before {
+                  content: '';
+                  width: 20px;
+                  height: 20px;
+                  border-radius: 50%;
+                  background-color: #eece68;
+                  opacity: 0.7;
+                  animation: scale 2s infinite cubic-bezier(0, 0, 0.49, 1.02);
+                  animation-delay: 400ms;
+                  transition: 0.5s all ease;
+                  transform: scale(1);
+                }
+
+                .item-3 {
+                  width: 20px;
+                  height: 20px;
+                  border-radius: 50%;
+                  background-color: #eec368;
+                  margin: 7px;
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                }
+                @keyframes scale {
+                  0% {
+                    transform: scale(1);
+                  }
+                  50%,
+                    75% {
+                    transform: scale(2.5);
+                  }
+                  78%, 100% {
+                    opacity: 0;
+                  }
+                }
+                .item-3:before {
+                  content: '';
+                  width: 20px;
+                  height: 20px;
+                  border-radius: 50%;
+                  background-color: #eec368;
+                  opacity: 0.7;
+                  animation: scale 2s infinite cubic-bezier(0, 0, 0.49, 1.02);
+                  animation-delay: 600ms;
+                  transition: 0.5s all ease;
+                  transform: scale(1);
+                }
+
+                .item-4 {
+                  width: 20px;
+                  height: 20px;
+                  border-radius: 50%;
+                  background-color: #eead68;
+                  margin: 7px;
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                }
+                @keyframes scale {
+                  0% {
+                    transform: scale(1);
+                  }
+                  50%,
+                    75% {
+                    transform: scale(2.5);
+                  }
+                  78%, 100% {
+                    opacity: 0;
+                  }
+                }
+                .item-4:before {
+                  content: '';
+                  width: 20px;
+                  height: 20px;
+                  border-radius: 50%;
+                  background-color: #eead68;
+                  opacity: 0.7;
+                  animation: scale 2s infinite cubic-bezier(0, 0, 0.49, 1.02);
+                  animation-delay: 800ms;
+                  transition: 0.5s all ease;
+                  transform: scale(1);
+                }
+
+                .item-5 {
+                  width: 20px;
+                  height: 20px;
+                  background: #f583a1;
+                  border-radius: 50%;
+                  background-color: #ee8c68;
+                  margin: 7px;
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                }
+                @keyframes scale {
+                  0% {
+                    transform: scale(1);
+                  }
+                  50%,
+                    75% {
+                    transform: scale(2.5);
+                  }
+                  78%, 100% {
+                    opacity: 0;
+                  }
+                }
+                .item-5:before {
+                  content: '';
+                  width: 20px;
+                  height: 20px;
+                  border-radius: 50%;
+                  background-color: #ee8c68;
+                  opacity: 0.7;
+                  animation: scale 2s infinite cubic-bezier(0, 0, 0.49, 1.02);
+                  animation-delay: 1000ms;
+                  transition: 0.5s all ease;
+                  transform: scale(1);
+                }
+
+                </style>
+                <div class="preloader">
+                  <div class="preloader__container">
+                    <div class="item-1"></div>
+                    <div class="item-2"></div>
+                    <div class="item-3"></div>
+                    <div class="item-4"></div>
+                    <div class="item-5"></div>
+                  </div>
+                </div>`);
+    };
+
+    const removeStatusMessage = () => {
+      const status = document.querySelector('.status-message');
+      if (!status) return;
+        setTimeout(() => {
+        status.remove();
+      }, 5000);
+    };
 
     const postData = (body, outputData, errorData) => {
       const request = new XMLHttpRequest();
@@ -393,21 +610,16 @@ window.addEventListener("DOMContentLoaded", () => {
 
       request.send(JSON.stringify(body));
     };
-
     forms.forEach(form => {
-
       form.addEventListener('input', (evt) => {
         let target = evt.target;
 
-
         if (target.name === 'user_phone') {
           target.value = target.value.replace(/[^\+\d]/g, '');
-          console.log(target.value);
         }
 
         if (target.name === 'user_name' || target.name === 'user_message') {
           target.value = target.value.replace(/[^а-я ]/gi, '');
-          console.log(target.value);
         }
       });
 
@@ -417,18 +629,25 @@ window.addEventListener("DOMContentLoaded", () => {
 
         const formData = new FormData(form);
         statusMessage.textContent = loadMessage;
+
+        bodyHtml.insertAdjacentHTML(`beforeend`, loader());
+        const loaderHtml = document.querySelector('.preloader');
+
         let body = {};
         for (let val of formData.entries()) {
           body[val[0]] = val[1];
         }
         postData(body,
           () => {
+            removeStatusMessage();
             statusMessage.textContent = successMessage;
             form.reset();
+            loaderHtml.remove();
           },
           (error) => {
-            console.log(error);
+            removeStatusMessage();
             statusMessage.textContent = errorMessage;
+            loaderHtml.remove();
           });
       });
     });
