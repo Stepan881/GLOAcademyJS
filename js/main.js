f54ce80b170eba4580376f8723ad8749618a6e46
stepan881 <stepanstepanov180@gmail.com>
2020-05-01 07:08:32 +0300

    validate tel

--- js/main.js
+++ js/main.js
@@ -610,24 +610,67 @@ window.addEventListener("DOMContentLoaded", () => {
 
 
     };
+
+
+    const errorInput = (inp, text) => {   
+      let div = inp.parentNode; 
+      div = div.querySelector('.input-error');
+      if (div) { 
+        return;   
+      }
+      let idTimer;
+      const error = `
+        <div class="input-error" style="color: red;">
+        ${text}
+        </div>
+      `;
+      inp.style.border = '2px solid red';
+      inp.insertAdjacentHTML('beforebegin', error);
+      const inputError = document.querySelector('.input-error');
+      setTimeout(() => {
+        inputError.remove();
+        inp.removeAttribute('style');
+      }, 5000);
+    };
+
+    const validateTel = (tel) => {
+      let str = tel[0].value.replace('+', '').length;
+      if (str < 8) {
+        errorInput(tel[0], `Минимум 8 цифр`);
+        return false;
+      } else {
+        return true;
+      }
+
+    };
+
     forms.forEach(form => {
+
       form.addEventListener('input', (evt) => {
         let target = evt.target;
         if (target.name === 'user_phone') {
+          if (target.style) {
+            target.style.border = 'none';
+          }
           target.value = target.value.replace(/[^\+\d]/g, '');
-        }
+          if (!/^\+?(\d){0,18}$/g.test(target.value)) {
+            target.value = target.value.substring(0, target.value.length - 1);
+          }
 
+        }
         if (target.name === 'user_name' || target.name === 'user_message') {
           target.value = target.value.replace(/[^а-я ]/gi, '');
         }
       });
 
-
-
-
-
       form.addEventListener('submit', (event) => {
         event.preventDefault();
+        const firmTel = [...event.target.elements].filter((item) => item.name === 'user_phone');
+        if (!validateTel(firmTel)) {
+          return;
+        } 
+        
+        
         form.appendChild(statusMessage);
         statusMessage.style.cssText = `font-size: 2rem;
               color: #fff; `;
@@ -658,7 +701,6 @@ window.addEventListener("DOMContentLoaded", () => {
             statusMessage.textContent = errorMessage;
             loaderHtml.remove();
         };
-
 
         postData(body)
           .then(outputData)
