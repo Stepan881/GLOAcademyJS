61afe8ce8b03aaf1be9d3545470b3cf2688a5992
stepan881 <stepanstepanov180@gmail.com>
2020-05-07 21:22:51 +0300

    create modal animate

--- src/modules/togglePopup.js
+++ src/modules/togglePopup.js
@@ -1,16 +1,33 @@
 const togglePopup = () => {
   const popup = document.querySelector('.popup');
+  const popupContent = document.querySelector('.popup-content');
   const popupBtn = document.querySelectorAll('.popup-btn');
+
+  let count = -200;
+  const animate = () => {
+      if (document.documentElement.clientWidth < 768) {
+        popupContent.style.transform = `translate(0)`;
+          return;
+      }
+      let requestId = requestAnimationFrame(animate);
+      count += 5;
+      popupContent.style.transform = `translate(${count}%)`;
+      if (count >= 0) {
+          cancelAnimationFrame(requestId);
+          
+      }
+  };
 
   popupBtn.forEach(el => {
       el.addEventListener('click', () => {
           popup.style.display = 'block';
+          animate();
       });
   });
 
   popup.addEventListener('click', (event) => {
     let target = event.target;
-
+    count = -200; 
     if (target.classList.contains('popup-close')) {
       popup.style.display = 'none';
     } else {
@@ -20,6 +37,8 @@ const togglePopup = () => {
       }
     }
   });
+
+
 };
 
 export default togglePopup;
