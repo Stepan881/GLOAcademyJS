138bf5d19ed9cb1a5bfd40faa3924d36c301e00c
stepan881 <stepanstepanov180@gmail.com>
2020-05-08 12:56:32 +0300

    lesson28 IE11

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 'use strict';
 
-
+import 'url-polyfill';
 import "regenerator-runtime/runtime";
+import 'element-remove';
 import "@babel/polyfill";
 import 'nodelist-foreach-polyfill';
 import 'formdata-polyfill';
 import 'element-closest';
 import 'es6-promise';
-import 'fetch-polyfill';
-
+// import 'fetch-polyfill';
+import 'element-closest/browser';
+import 'whatwg-fetch';
+import "scroll-behavior-polyfill";
 
 import countTimer from './modules/countTimer';
 import toggleMenu from './modules/toggleMenu';
